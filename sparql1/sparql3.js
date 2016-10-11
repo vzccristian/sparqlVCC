0f2d8c68346e8e45d81c6561694de3c809194cc5
vzccristian <vzccristian@gmail.com>
2016-10-11 13:27:45 +0200

    Value funciona. Checkbox...

--- sparql1/sparql3.js
+++ sparql1/sparql3.js
@@ -2,10 +2,10 @@ function getRecursos(){
 	console.log("Tomando recursos...");
 	document.getElementById("divRecursos").innerHTML="";
 	// Obtengo el punto sparql al que se quiere acceder.
-    var listaEndPoint =  document.getElementById("lista");
-    var endpoint = listaEndPoint.options[listaEndPoint.selectedIndex].value;
-    var queryGraph = "";
-    if (endpoint=="http://opendata.caceres.es/sparql/") {
+    var listaEndPoint =  document.getElementById("lista"); //Seleccionar lista para consultas open data
+    endpointGeneral = listaEndPoint.options[listaEndPoint.selectedIndex].value;
+    queryGraph = "";
+    if (endpointGeneral=="http://opendata.caceres.es/sparql/") {
     	    var sparqlQuery =   "select distinct ?Concept where {[] a ?Concept"+
      " FILTER regex(?Concept , \"ontomunicipio\")} ";
     }
@@ -17,15 +17,14 @@ function getRecursos(){
    console.log(sparqlQuery);
     $.ajax({
      	data:{"default-graph-uri":queryGraph, query:sparqlQuery, format:'json'},
-        url: endpoint,
+        url: endpointGeneral,
         cache: false,
         statusCode: {400: function(error){alert("ERROR");}  },
 
         success : function(data) {
         //Introducir desplegable con todas las posibles opciones.
 		var datos = data.results.bindings;
-		var columName[];
-		columName[0]="Concept";
+		var columName="Concept";
 		crearDesplegable(datos,"#divRecursos",columName)
 		}
 	});
@@ -36,54 +35,65 @@ function crearDesplegable(datos,divInsertar,columName) {
 		var div = document.querySelector(divInsertar),
 		frag = document.createDocumentFragment(),
 		select = document.createElement("select");
-		var valor = datos[0][columName[0]].value;
-		var valorLimpio = valor.split("#");
-		select.options.add(new Option(valorLimpio[1], "AU", true, true));
-		for (var i = 1; i < datos.length; i++) {
-			valor = datos[i][columName[0]].value;
+		var valor = "";
+		for (var i = 0; i < datos.length; i++) {
+			valor = datos[i][columName].value;
 			valorLimpio = valor.split("#");
 			select.options.add(new Option(valorLimpio[1], "AU"));
 		}
-
 		frag.appendChild(select);
 		div.appendChild(frag);
 		espacio = document.createTextNode("      ");
  		divRecursos.appendChild(espacio);
 		createButton(divRecursos,null,"Seleccionar recurso","selectResources");
-		document.getElementById("selectResources").onclick = function() {getPropertys()};
+		document.getElementById("selectResources").onclick = function() {getPropertiesOfResources()};
 }
-function getPropertys() {
+
+function getPropertiesOfResources() {
 	console.log("Tomando propiedades...");
 	document.getElementById("divPropiedades").innerHTML="";
-	// Obtengo el punto sparql al que se quiere acceder.
-    var listaEndPoint =  document.getElementById("lista");
-    var endpoint = listaEndPoint.options[listaEndPoint.selectedIndex].value;
-    var queryGraph = "";
     var sparqlQuery =   "select distinct ?property where {"+
          "?instance a om:Museo . "+
          "?instance ?property ?obj . }";
     console.log(sparqlQuery);
 	$.ajax({
-		data : {
-			"default-graph-uri" : queryGraph,
-			query : sparqlQuery,
-			format : 'json'
-		},
-		url : endpoint,
-		cache : false,
-		statusCode : {
-			400 : function(error) {
-				alert("ERROR");
-			}
-		},
+     	data:{"default-graph-uri":queryGraph, query:sparqlQuery, format:'json'},
+        url: endpointGeneral,
+        cache: false,
+        statusCode: {400: function(error){alert("ERROR");}  },
 		success : function(data) {
 			console.log(data);
 			var datos = data.results.bindings;
-			generarTabla(datos);
+			generarForm(datos,"property","divPropiedades");
 		}
 	});
 
 }
+
+function generarForm(datos,columName,div) {
+	for ( var i in datos) {
+		//Checkbox
+		var valor = datos[i][columName].value;
+		if (valor.search("#")!=-1) { 
+		var valorLimpio = valor.split("#");
+		var checkbox = document.createElement('input');
+		checkbox.type = "checkbox";
+		checkbox.name = valorLimpio[1];
+		checkbox.value = valorLimpio[1];
+		checkbox.id = valorLimpio[1];
+		//Etiqueta
+		var label = document.createElement('label');
+		label.htmlFor = "id";
+		label.appendChild(document.createTextNode(valorLimpio[1]));
+		var pElement = document.createElement("p");
+		pElement.appendChild(checkbox);
+		pElement.appendChild(label);
+		var container=document.getElementById(div);
+		container.appendChild(pElement);
+		}
+	}	
+}
+
 function createButton(context, func, valor, id){
     var button = document.createElement("input");
     button.type = "button";
@@ -141,36 +151,30 @@ function getDataConsulta() {
             },   
 		  success : function(data) {
 		  	console.log(data);
-		  				var datos = data.results.bindings;
-			generarTabla(datos);
+		  	var datos = data.results.bindings;
+		  	var columName="nomFarma";
+			generarTabla(datos,columName);
 		}
         });
 
     }
    
-   function generarTabla(datos) {
-
-		//GENERACION DE TABLA
+   function generarTabla(datos,columName) {
+		//GENERACION DE TABLA A MENOS QUE YA HAYA SIDO CREADA. 
 		if (document.getElementById("tablaSPARQL")==null) {
 		// Obtener la referencia del elemento body
 		var body = document.getElementById("divTabla");
-		
 		// Crea un elemento <table> y un elemento <tbody>
 		var tabla   = document.createElement("table");
 		tabla.id=("tablaSPARQL");
 		var tblBody = document.createElement("tbody");
-		
-		
 		for ( var i in datos) {
 			// Crea las hileras de la tabla
 		    var hilera = document.createElement("tr");
 		    var celda = document.createElement("td");
-		 //  var nom=datos[i];
-		 //TODO PROBLEMAS PARA COGER LAS PROPIEDADES.
-
 		    console.log(datos[i]);
-		    
-		    var valor = datos[i].value;
+		    console.log(columName);
+		    var valor = datos[i][columName].value;
 		    var textoCelda = document.createTextNode(valor);
 		    celda.appendChild(textoCelda);
 		    hilera.appendChild(celda);
