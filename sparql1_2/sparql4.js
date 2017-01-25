2d84b2253e2e0fcd2a4fd4dddc214bc94e8f9ab4
vzccristian <vzccristian@gmail.com>
2017-01-25 12:23:20 +0100

    Limpiando código

--- sparql1_2/sparql4.js
+++ sparql1_2/sparql4.js
@@ -1,33 +1,30 @@
 
-function getRecursos(posicion){
+function getResources(posicion){
 	console.log("Tomando recursos...");
 	document.getElementById("divPropiedades"+posicion).innerHTML="";
 	document.getElementById("divTabla"+posicion).innerHTML="";
 	document.getElementById("textAreaConsulta"+posicion).value="";
 	document.getElementById("divRecursos"+posicion).innerHTML="";
 	var listaEndPoint =  document.getElementById("lista"+posicion); //Seleccionar lista para consultas open data
-    endpointGeneral = listaEndPoint.options[listaEndPoint.selectedIndex].value; //GLOBAL -> SELECCIONAR PUNTO DE CONSULTA SELECCIONADO EN DESPLEGABLE.
+    var endpointGeneral = listaEndPoint.options[listaEndPoint.selectedIndex].value; 
     queryGraph = ""; // GLOBAL -> NUNCA CAMBIA
     var sparqlQuery="select distinct ?Concept where {[] a ?Concept FILTER isURI(?Concept )} ";
 
    //CALLBACK PARA ESPERAR POR LA CONSULTA.
-	var callbackQuery = function (data) {
-		if ( data!== null) { //Consulta CORRECTA
-			datosRecursos=data; //GLOBAL
-			var columName="Concept";
-			crearDesplegableRecursos(data,"divRecursos",columName,posicion);
-		}
+	var callbackQuery = function (resourceData) {
+		if ( resourceData!== null)  //Consulta CORRECTA
+			createResourceList(resourceData,"divRecursos","Concept",posicion);
 	};
-	lanzarConsultaSPARQL(sparqlQuery,endpointGeneral,callbackQuery);
+	launchSPARQLQuery(sparqlQuery,endpointGeneral,callbackQuery);
 }
 
 
-function lanzarConsultaSPARQL(sparqlQuery,puntoDeConsulta,callback) {
+function launchSPARQLQuery(sparqlQuery,endpointGeneral,callback) {
 	var queryGraph = "";
 	dataQuery = null;
 	    $.ajax({
      	data:{ query:sparqlQuery,format:'application/sparql-results+json'},
-        url: puntoDeConsulta,
+        url: endpointGeneral,
         cache: false,
       	// crossDomain: true,
       	dataType: 'json',
@@ -43,10 +40,10 @@ function lanzarConsultaSPARQL(sparqlQuery,puntoDeConsulta,callback) {
 
 }
 
-// function lanzarConsultaSPARQL2(sparqlQuery,puntoDeConsulta,callback) {
+// function launchSPARQLQuery2(sparqlQuery,puntoDeConsulta,callback) {
 // 	var queryGraph = "";
 // 	dataQuery = null;
-// 	console.log("lanzarConsultaSPARQL");
+// 	console.log("launchSPARQLQuery");
 // 	var test="hola";
 // 	console.log(sparqlQuery);
 // 	    $.ajax({
@@ -70,15 +67,15 @@ function lanzarConsultaSPARQL(sparqlQuery,puntoDeConsulta,callback) {
 
 // }
 
-function crearDesplegableRecursos(datos,divInsertar,columName,posicion) {
-		var div = document.getElementById(divInsertar+posicion),
+function createResourceList(resourceData,divToInsert,columName,position) {
+		var div = document.getElementById(divToInsert+position),
 		frag = document.createDocumentFragment(),
 		select = document.createElement("select");
-		console.log(divInsertar+posicion);
-		select.id="desplegableRecursos"+posicion;
+		console.log(divToInsert+position);
+		select.id="resourceList"+position;
 		var valor = "";
-		for (var i = 0; i < datos.length; i++) {
-			valor = datos[i][columName].value;
+		for (var i = 0; i < resourceData.length; i++) {
+			valor = resourceData[i][columName].value;
 			//valorLimpio = valor.split("#");
 			select.options.add(new Option(valor, valor));
 		}
@@ -87,59 +84,60 @@ function crearDesplegableRecursos(datos,divInsertar,columName,posicion) {
 		espacio = document.createTextNode("      ");
  		div.appendChild(espacio);
 
-		createButton(div,null,"Seleccionar recurso","selectResources",posicion);
-		document.getElementById("buttonselectResources"+posicion).onclick = function() {
-		getPropertiesOfResources(posicion); };
+		createButton(div,null,"Seleccionar recurso","selectResources",position);
+		document.getElementById("buttonselectResources"+position).onclick = function() {
+		getPropertiesOfResources(resourceData, position); };
 
 }
 
-function getPropertiesOfResources(posicion) {
-	console.log("getPropertiesOfResources() "+posicion);
-	document.getElementById("divPropiedades"+posicion).innerHTML="";
-	document.getElementById("divTabla"+posicion).innerHTML="";
-	document.getElementById("textAreaConsulta"+posicion).value="";
-	var	resource=document.getElementById("desplegableRecursos"+posicion);
+function getPropertiesOfResources(resourceData, position) {
+	console.log("getPropertiesOfResources() "+position);
+	document.getElementById("divPropiedades"+position).innerHTML="";
+	document.getElementById("divTabla"+position).innerHTML="";
+	document.getElementById("textAreaConsulta"+position).value="";
+	var	resource=document.getElementById("resourceList"+position);
 
 	if(resource.selectedIndex<0)
     		alert('Error');
 	else {
-   	 	var valorSeleccionado=datosRecursos[resource.selectedIndex].Concept.value;
-   	 	console.log(datosRecursos[resource.selectedIndex].Concept.value);
+   	 	var selectedValue=resourceData[resource.selectedIndex].Concept.value;
+   	 	console.log(resourceData[resource.selectedIndex].Concept.value);
    		var sparqlQuery =   "select distinct ?property where {"+
-         "?instance a <"+valorSeleccionado+"> . "+
+         "?instance a <"+selectedValue+"> . "+
          "?instance ?property ?obj . }";
         console.log("Consulta para atributos :" + sparqlQuery);
 
-    var callbackQueryProperties = function (data) {
-		if (data!==null) { //Consulta CORRECTA
-			generarForm(data,"property","divPropiedades",posicion);
+    var callbackQueryProperties = function (propData) {
+		if (propData!==null) { //Consulta CORRECTA
+			var div=document.getElementById("divPropiedades"+position);
+			generateCheckBox(propData,resourceData,"property",div,position);
 		}
 	};
-    lanzarConsultaSPARQL(sparqlQuery,endpointGeneral,callbackQueryProperties);    
+    launchSPARQLQuery(sparqlQuery,endpointGeneral,callbackQueryProperties);    
 	}
 }
 
-function generarForm(datos,columName,div,posicion) {
-	console.log("generarForm en Posición: "+ posicion);
-	datosOntologia=[];
+function generateCheckBox(propData,resourceData,columName,div,posicion) {
+	console.log("generateCheckBox en Posición: "+ posicion);
+	var propOntologyData=[];
 	var valorLimpio=null;
-	for ( var i in datos) {
-		var valor = datos[i][columName].value; //Contiene todo el string.
+	for ( var i in propData) {
+		var valor = propData[i][columName].value; //Contiene todo el string.
 		if ( valor.search("#")!=-1) { 
 			valorLimpio = valor.split("#");
-			generarForm2(valor,"#",valorLimpio.length-1,div,posicion);
+			generateCheckBox2(valor,"#",valorLimpio.length-1,div,posicion);
 		} else {
 			valorLimpio = valor.split("/");
-			generarForm2(valor,"/",valorLimpio.length-1,div,posicion);
+			generateCheckBox2(valor,"/",valorLimpio.length-1,div,posicion);
 		}
 		//Creo array con ontologia y atributo.
-		datosOntologia.push({ ontologia: valor, atributo: valorLimpio[valorLimpio.length-1] });
+		propOntologyData.push({ ontologia: valor, atributo: valorLimpio[valorLimpio.length-1] });
 	}	
 	// function(){ return changeViewMode(myvar); }
-	createButton(container,function(){ return construirConsulta(posicion); },"Consultar en punto de consulta","buttonConstruirConsulta",posicion);
+	createButton(div,function(){ return buildQuery(resourceData,posicion,propOntologyData); },"Consultar en punto de consulta","buttonbuildQuery",posicion);
 }
 
-function generarForm2(valor,separador,posicion,div,posicionBody) {
+function generateCheckBox2(valor,separador,posicion,div,posicionBody) {
 	var valorLimpio = valor.split(separador);
 	var checkbox = document.createElement('input');
 	checkbox.type = "checkbox";
@@ -153,8 +151,7 @@ function generarForm2(valor,separador,posicion,div,posicionBody) {
 	label.appendChild(document.createTextNode(valorLimpio[posicion]));
 	pElement.appendChild(checkbox);
 	pElement.appendChild(label);
-	container=document.getElementById(div+posicionBody);
-	container.appendChild(pElement);
+	div.appendChild(pElement);
 }
 
 
@@ -173,69 +170,66 @@ function getCheckedBoxes(chkboxName) {
   return checkboxesChecked.length > 0 ? checkboxesChecked : null;
 }
 
-
-var tester;
-function construirConsulta(posicion) {
+function buildQuery(resourceData,position,propOntologyData) {
 	console.log("Construyendo consulta");
-	var	container=document.getElementById("divPropiedades"+posicion);
-	var	resource=document.getElementById("desplegableRecursos"+posicion);
-	var	textArea=document.getElementById("textAreaConsulta"+posicion);
-
-	var checkedBoxes = getCheckedBoxes("checkboxForm"+posicion);
+	var	container=document.getElementById("divPropiedades"+position);
+	var	resource=document.getElementById("resourceList"+position);
+	var	textArea=document.getElementById("textAreaConsulta"+position);
+	var checkedBoxes = getCheckedBoxes("checkboxForm"+position);
 	var checkedValue = null; 
 	if (checkedBoxes!==null) {
-	tester=checkedBoxes;
-	consulta="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n "+
+	var query="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n "+
 				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n "+
 				"PREFIX owl: <http://www.w3.org/2002/07/owl#> \n "+
 				"PREFIX dc: <http://purl.org/dc/elements/1.1/> \n " +
-				"PREFIX om: <http://opendata.caceres.edds/def/ontomunicipio> \n "+
+				// "PREFIX om: <http://opendata.caceres.edds/def/ontomunicipio> \n "+
 				"Select "
 				;
 
 	for(var i=0; checkedBoxes[i]; ++i){
-		consulta+="?var"+checkedBoxes[i].value+" ";
+		query+="?var"+checkedBoxes[i].value+" ";
       }
-		consulta+="where { \n";
+		query+="where { \n";
 
-	var valorSeleccionado=datosRecursos[resource.selectedIndex]["Concept"].value;
-	consulta+="?x a <"+valorSeleccionado+"> . \n";
+	var valorSeleccionado=resourceData[resource.selectedIndex]["Concept"].value;
+	query+="?x a <"+valorSeleccionado+"> . \n";
+
 	//ATRIBUTOS
 	for(var j=0; checkedBoxes[j]; ++j){
-		consulta+="?x ";
-     // datosOntologia
+		query+="?x ";
+     // propOntologyData
      encontrado=false;
      var ontURL=null;
-      for (i = 0; i < datosOntologia.length && !encontrado; i++) {
-      	if (datosOntologia[i].atributo==checkedBoxes[j].value) {
-      		ontURL=datosOntologia[i].ontologia;
+      for (i = 0; i < propOntologyData.length && !encontrado; i++) {
+      	if (propOntologyData[i].atributo==checkedBoxes[j].value) {
+      		ontURL=propOntologyData[i].ontologia;
       		encontrado=true;
      		}
      	 }
      	if (encontrado)
-     		consulta+="<"+ontURL+"> ?var"+checkedBoxes[j].value+" . \n";
+     		query+="<"+ontURL+"> ?var"+checkedBoxes[j].value+" . \n";
 	}
-	consulta+=" }";
-
-	textArea.value=consulta;
-	consulta+=" LIMIT 500";
-	enviarConsulta(consulta,checkedBoxes,posicion);
+	query+=" }";
+	query+=" LIMIT 500";
+	textArea.value=query;
+	sendQuery(query,checkedBoxes,position);
 	} else {
 		console.log("**ERROR** No hay atributos seleccionados.");
 	}
 }
 
-function enviarConsulta(consulta,encabezados,posicionBody) {
+function sendQuery(query,encabezados,posicionBody) {
 	console.log("Enviando consulta...");
 	var callbackSendQuery = function (data) {
 		if (data!==null) { //Consulta CORRECTA
 			generarTabla(data,encabezados,posicionBody);
 		}
 	};
-    lanzarConsultaSPARQL(consulta,endpointGeneral,callbackSendQuery);  
+    launchSPARQLQuery(query,endpointGeneral,callbackSendQuery);  
 }
 
 function createButton(context, func, valor, id,posicion){
+	console.log(context);
     var button = document.createElement("input");
     button.type = "button";
     button.value = valor+" "+posicion;
@@ -245,14 +239,14 @@ function createButton(context, func, valor, id,posicion){
 }
 
 
-function generarTabla(datos,encabezados,posicionBody) {
+function generarTabla(propData,encabezados,posicionBody) {
 	console.log("Generando tabla...");
-	datosTabla=datos; //GLOBALES TABLA Y ENCABEZADOS.
+	propDataTabla=propData; //GLOBALES TABLA Y ENCABEZADOS.
 	encabezadosTabla=encabezados;
 
 	var divActual="divTabla"+posicionBody; 
 	var tablaActual="tablaSPARQL"+posicionBody;
-
+	document.getElementById("divTabla"+posicionBody).innerHTML="";
 	var borrar= document.getElementById(tablaActual);
 	if (borrar!==null)
 		document.getElementById(tablaActual).innerHTML="";
@@ -277,12 +271,12 @@ function generarTabla(datos,encabezados,posicionBody) {
 	}
 
 	//CREACION DE TABLA
-	for ( var i in datos) {
+	for ( var i in propData) {
 		hilera = document.createElement("tr");
 		for  (var j in encabezados) {
 		    var celda = document.createElement("td");
 		    var atrib="var"+encabezados[j].value;
-		    var valor = datos[i][atrib].value;
+		    var valor = propData[i][atrib].value;
 		    console.log(valor);
 		    var textoCelda = document.createTextNode(valor);
 		    celda.appendChild(textoCelda);
